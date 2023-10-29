afcf30d21d13bed0a6e9d8d7effe0e8ad427cf65
Gary Yu <hyu100@my.bcit.ca>
2023-10-28 23:09:32 -0700

    create picture slider

---
+++ script.js
@@ -0,0 +1,40 @@
+const track = document.getElementById("image-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmousemove = e => {
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+    const maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+    if (nextPercentage > 0) {
+        nextPercentage = 0;
+    } else if (nextPercentage < -100) {
+        nextPercentage = -100;
+    }
+
+    track.dataset.percentage = nextPercentage;
+
+    //track.style.transform = `translate(${nextPercentage}%, -50%)`;
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, {duration: 1200, fill: "forwards"});
+
+    for(const image of track.getElementsByClassName("image")) {
+        //image.style.objectPosition = `${nextPercentage + 100}% 50%`;
+        image.animate({
+            objectPosition: `${100 + nextPercentage}% center`
+        }, {duration: 1200, fill: "forwards"});
+    }
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
